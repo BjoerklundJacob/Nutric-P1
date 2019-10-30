d26336583fcd07be56cb1f0ca107805b9644d67d
Casper <casper-s@hotmail.dk>
2019-10-30 09:34:15 +0100

    Update Casper.c

--- Casper.c
+++ Casper.c
@@ -32,9 +32,9 @@ int main (){
 
    /* write 10 lines of text into the file stream*/
    for(i = 0; i < 7;i++){
-       fprintf(fp, "Dag: %d   (2. personers)\n",i);
+       fprintf(fp, "Dag: %d \n",i);
        fprintf(fp, "---------------------------\n");
-       fprintf(fp, "Opskrift %d: %s\n", i, retter [i]);
+       fprintf(fp, "Opskrift %d: %s (2. pers)(45. min)\n", i, retter [i]);
        fprintf(fp, ">  %s\n", ingredienser[i]);
        fprintf(fp, ">  %s\n", ingredienser[i]);
        fprintf(fp, "\n");
