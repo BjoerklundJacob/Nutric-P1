b0f00b0b10ff67aa7650b7e2669fca39be2001c4
Jacob <35376760+BjoerklundJacob@users.noreply.github.com>
2019-10-30 14:47:56 +0100

    Moved typedefs to types.h and finished current state of recipe_sets functions

--- recipe_sets.c
+++ recipe_sets.c
@@ -1,67 +1,82 @@
 #include <stdio.h>
 #include <math.h>
 
-#define RECIPE_COUNT 3
+#define RECIPE_COUNT 33
 #define INGREDIENT_COUNT 10
-#define RECIPE_SET_LEN ceil((double) RECIPE_COUNT / (double) (sizeof(int)*8))
+/* RECIPE_SET_LEN is equal to the amount of int's needed to have a bitflag for each recipe */
+#define RECIPE_SET_LEN (int) ( ((double) RECIPE_COUNT * 2.0 - 1.0) / (double) (sizeof(int)*8*2)+1 )
 #define LEN(x) (sizeof(x)/sizeof(x[0]))
 
-typedef struct _RECIPE{
-    char *name;
-    int prep_time;
-    int cook_time;
+#include "types.h"
 
-    int servings;
-
-    int ingredients[INGREDIENT_COUNT];/*int array with all ingredients (value of 0 if ingredient is not in recipe)*/
-
-    int instructions;
-    char **instructions;/*instructions string array*/
-  }RECIPE;
-
-  typedef struct _RECIPE_SET{
-    char tag;
-    int bit_string[RECIPE_SET_LEN];
-  }RECIPE_SET;
-
-enum eRecipeSetTags{
-  tomato,
-  nut,
-  gluten,
-  soya,
-  e_recipe_set_tags_size,
-  terminator
-};
+void find_recipes(RECIPE_SET* recipe_set, RECIPE_SET *recipe_sets, eRECIPE_SET_TAGS *tags, int tag_count);
+void recipe_junction(RECIPE_SET* recipe1, RECIPE_SET recipe2);
+void print_recipe_set(RECIPE_SET recipe_set);
+RECIPE_SET recipe_set_bit_flip(RECIPE_SET* recipe_set);
 
 int main(void){
 
   RECIPE recipes[RECIPE_COUNT];
   RECIPE_SET recipe_sets[e_recipe_set_tags_size];
+  RECIPE_SET recipe_set;
 
   int i, j;
-  char *tags;
+  eRECIPE_SET_TAGS tags[] = {nut, gluten};
 
-  /* Initialise all recipe set bit strings to full 0's */
-  for(j = 0; j < e_recipe_set_tags_size; ++j)
+  /* Initialise all recipe set bit strings to "random" test values" */
+  for(j = 0; j < (int) e_recipe_set_tags_size; ++j)
     for(i = 0; i < RECIPE_SET_LEN; ++i)
-      recipe_sets[j].bit_string[i] = 0;
+      recipe_sets[j].bit_string[i] = (345 << j) + 9599*j;
 
-  find_recipes(recipe_set, tags);
+  /* Initialise recipe set bit string to full 1's */
+  for(i = 0; i < RECIPE_SET_LEN; ++i)
+    recipe_set.bit_string[i] = -1;
 
+  printf("Bit string length = %d\n", RECIPE_SET_LEN);
+  for(i = 0; i < (int) e_recipe_set_tags_size; ++i){
+    printf("Printing recipe set %d\n", i);
+    print_recipe_set(recipe_sets[i]);
+    printf("\n\n");
+  }
+
+  find_recipes(&recipe_set, recipe_sets, tags, LEN(tags));
+
+  printf("Available recipes from both set 0 and set 1:\n");
+  print_recipe_set(recipe_set_bit_flip(&recipe_set));
+  printf("\n");
 
   return 0;
 }
 
-void find_recipes(int* recipe_set, char* tags){
-  int i, tag_count;
-  int *recipe;
+void find_recipes(RECIPE_SET* recipe_set, RECIPE_SET *recipe_sets, eRECIPE_SET_TAGS *tags, int tag_count){
+  int i;
+  RECIPE_SET recipe;
   
-  tag_count = LEN(tags);
   for(i = 0; i < tag_count; ++i){
+    recipe = recipe_sets[tags[i]];
     recipe_junction(recipe_set, recipe);
   }
 }
 
-void recipe_junction(int* recipe1, int* recipe2){
+void recipe_junction(RECIPE_SET* recipe_set1, RECIPE_SET recipe_set2){
+  int i;
+  for(i = 0; i < RECIPE_SET_LEN; ++i)
+    recipe_set1->bit_string[i] &= recipe_set2.bit_string[i];
+}
 
+void print_recipe_set(RECIPE_SET recipe_set){
+  int i, j;
+  /* Loop for every bit in a recipe set bit string */
+  for(i = 0; i < RECIPE_SET_LEN; ++i){
+    for(j = sizeof(int)*8 - 1; j >= 0; --j){
+      printf("%c", (recipe_set.bit_string[i] & (1 << j)) ? '1' : '0');
+    }
+  }
+}
+
+RECIPE_SET recipe_set_bit_flip(RECIPE_SET* recipe_set){
+  int i;
+  for(i = 0; i < RECIPE_SET_LEN; ++i)
+    recipe_set->bit_string[i] = ~recipe_set->bit_string[i];
+  return *recipe_set;
 }
