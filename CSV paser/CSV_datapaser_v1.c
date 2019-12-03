6b66fa051379f52acb59884fc11af48b9c18916a
Casper <casper-s@hotmail.dk>
2019-12-03 09:32:22 +0100

    Update CSV_datapaser_v1.c

--- CSV paser/CSV_datapaser_v1.c
+++ CSV paser/CSV_datapaser_v1.c
@@ -2,14 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct nutrien_item{
+  char *ingredientname; 
+  double calcium; 
+  double iron; 
+  double zinc; 
+  double selenium; 
+  double vitamin_B2;
+  double vitamin_B3;
+  double vitamin_B12; 
+  double vitamin_A; 
+  double viatamin_D; 
+  double iodine;   
+
+
+};
+
 int main() {
+  FILE *f; 
 
-  return 0;
-}
+  fopen("IngredientsList.csv", "r"); 
 
-void cvs_reader(){
-  FILE *f;
-
+  
 
 
+  return EXIT_SUCCESS;
 }
