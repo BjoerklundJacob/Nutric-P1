cfd98eb7f43cb63774857d82f1206e48a3cd3482
Molitany <thor@beregaard.dk>
2019-12-05 09:09:39 +0100

    skraldespand :)

--- settings.c
+++ settings.c
@@ -1,5 +1,4 @@
 #include "settings.h"
-
 /**
  * Handles movement throughout the user settings whilst also giving descriptive text for the user.
  * @param userData the struct in control of all the users data and modifies the data
@@ -25,11 +24,6 @@ void UserSettings(UserData* userData){
             case '3':
                 ClearScreen();
                 Gender(userData);
-                UserSettingsText();
-                break;
-            case '4':
-                ClearScreen();
-                FoodExclusions(userData);
                 UserSettingsText();
                 break;
             case '0':
@@ -109,57 +103,7 @@ void Weight(UserData* userData){
     return;
 }
 
-/**
- * Allows the user to set and unset what ingredients must and must not be in the recipies.
- * @param userData the struct in control of all the users data and modifies the data
- */
-void FoodExclusions(UserData* userData) {
-    const char* stringTags[] = {"All", "Tomato", "Nuts", "Gluten", "Soya"};
-    int i, input;
-    char sign;
 
-    /* Gives the user directions on what is possible */
-    printf("Type the '+' to must include or '-' to exclude the respective ingredients or '*' to remove in- or exclusion.\n");
-    for (i = 1; i < e_recipe_set_tags_size; i++){
-        printf(" (%d) %s\n", i, stringTags[i]);
-    }
-    printf(" (0) Return to the User Settings\n\n");
-    
-    /* Checks the users input and puts it into the foodexclusions member array using -, + and a nonvalid number*/
-    do {
-        if (CheckInput(&sign, &input) == -1){
-            break;
-        }
-        for (i = 0; i < e_recipe_set_tags_size-1; i++){
-            if (i == input-1){        
-                if (sign == '-'){
-                    userData->foodExclusions[i] = -input;
-                }else if (sign == '+'){
-                    userData->foodExclusions[i] = input;
-                } else if (sign == '*'){
-                    userData->foodExclusions[i] = -e_recipe_set_tags_size;
-                    printf("Food Exclusion %s now added/updated\n", stringTags[input]);
-                    break;
-                }else{
-                    break;
-                }
-                printf("Food Exclusion %c%s now added/updated\n", sign, stringTags[abs(userData->foodExclusions[i])]);
-                break;
-            }
-        }
-    } while (input != 0);
-
-    /* Gives feedback to the user on what ingredients are in- and excluded */
-    printf("\nFood in- and exclusions are now\n");
-    for (i = 0; i < e_recipe_set_tags_size-1; i++){
-        if (userData->foodExclusions[i] == -e_recipe_set_tags_size){
-            printf("   %s\n", stringTags[i+1]);
-        }else{
-            printf("  %c%s\n", Signed(userData->foodExclusions[i]), stringTags[i+1]);
-        }
-    }
-    printf("\n");
-}
 
 /**
  * Prints the introductory text to user settings
--- settings.h
+++ settings.h
@@ -1,17 +1,15 @@
-#include "data.h"
-#include <ctype.h>
-#include <conio.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+#include "data.h"
 
 void UserSettings(UserData* userData);
 void Age(UserData* userData);
 void Gender(UserData* userData);
 void Weight(UserData* userData);
-void FoodExclusions(UserData* userData) ;
 void UserSettingsText(void);
 char Signed(int input);
 int CheckInput(char* sign, int* input);
 void ClearChar(char* sign);
-void ClearScreen(void);
 void SaveUserData(UserData userData);
