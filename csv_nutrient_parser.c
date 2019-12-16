d0cb080ec81e3b07da36416d9df2f9a38c109d5e
Sriimoont <57129400+Sriimoont@users.noreply.github.com>
2019-12-16 09:39:24 +0100

    Commented

--- csv_nutrient_parser.c
+++ csv_nutrient_parser.c
@@ -1,28 +1,5 @@
 #include "csv_nutrient_parser.h"
 
-<<<<<<< Updated upstream
-void get_ingredient_nutrients(ingredient_nutrients_t* output_array){
-  FILE *f = fopen("Ingredient_Nutrients.csv", "r"); 
-  int i;
-
-  if(f != NULL){
-    /* Reads the CSV-file and makes the nutrient arr */
-    for (i = 0; i < MAX_ARRAY_SIZE ; i++){
-
-      fscanf(f, " %[a-zA-Z() ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] ; %[a-zA-Z0-9. ] \n", 
-          output_array[i].ingredient_name, 
-          output_array[i].calcium, 
-          output_array[i].iodine,
-          output_array[i].iron,
-          output_array[i].selenium, 
-          output_array[i].zinc, 
-          output_array[i].vitamin_A,
-          output_array[i].vitamin_B2,
-          output_array[i].vitamin_B3, 
-          output_array[i].vitamin_B12, 
-          output_array[i].vitamin_D
-      );
-=======
 /**
   * Data parser to ingredientsList, makes a struct of ingredientsList  
   */
@@ -94,31 +71,17 @@ int get_ingredient_id(const char *search_string){
     while((chosen = choose_ingredient(options, search_string)) == -1){
       clear_screen();
       printf("Error, please input a valid option number.\n");
->>>>>>> Stashed changes
     }
-    fclose(f);
+    if(chosen != NO_RESULTS){
+      option = list_value(options, chosen);
+      id_val = atoi(map_value(option, "id"));
+    }
+  }
+  else{
+    printf("Ingredient nutrient data file could not be opened!\n");
+    exit(EXIT_FAILURE);
   }
 
-<<<<<<< Updated upstream
-}
-
-/*void printing_the_chosen_in(ingredient_nutrients_t ingredientsList[MAX_ARRAY_SIZE], int j){
-  printf("Ingredient: %d\n", j);
-      printf("-------------------------------------------------\n");
-      printf("Ingredients navn: %s\n", ingredientsList[j].ingredient_name);
-      printf("Calcium:          %s\n", ingredientsList[j].calcium);
-      printf("Iron:             %s\n", ingredientsList[j].iron); 
-      printf("Zinc:             %s\n", ingredientsList[j].zinc);
-      printf("Selenium:         %s\n", ingredientsList[j].selenium);
-      printf("Vitamin B2:       %s\n", ingredientsList[j].vitamin_B2);
-      printf("Vitamin B3:       %s\n", ingredientsList[j].vitamin_B3);
-      printf("Vitamin B12:      %s\n", ingredientsList[j].vitamin_B12);
-      printf("Vitamin A:        %s\n", ingredientsList[j].vitamin_A);
-      printf("Vitamin D:        %s\n", ingredientsList[j].vitamin_D);
-      printf("Iodine:           %s\n", ingredientsList[j].iodine);         
-      printf("\n\n"); 
-}*/
-=======
   /** 
     * Free allocated memory 
     */
@@ -259,4 +222,3 @@ ingredient_nutrients_t nutrient_array_to_struct(nutrient_arrays_t nutrients){
   free(nutrients.nutrient_id);
   return ingredient_nutrients;
 }
->>>>>>> Stashed changes
